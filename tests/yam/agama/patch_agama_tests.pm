61bbb5aec901c580ea634944325342ec6a3a17a4
Huajian.Luo <hluo@suse.com>
2025-10-21 19:23:49 +0800

    Use GitHub releases instead of pushing the bundler files to repo

--- tests/yam/agama/patch_agama_tests.pm
+++ tests/yam/agama/patch_agama_tests.pm
@@ -1,17 +1,20 @@
 ## Copyright 2024 SUSE LLC
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-# Summary: Patch Agama on Live Medium using yupdate in order to copy
+# Summary: Patch Agama on Live Medium using agama download.
 # integration test from GitHub.
 # Maintainer: QE YaST and Migration (QE Yam) <qe-yam at suse de>
 
 use base Yam::Agama::patch_agama_base;
-use testapi qw(assert_script_run get_required_var select_console);
+use testapi qw(assert_script_run select_console);
 
 sub run {
     select_console 'install-shell';
-    my ($repo, $branch) = split /#/, get_required_var('YUPDATE_GIT');
-    assert_script_run("AGAMA_TEST=" . get_required_var('AGAMA_TEST') . " yupdate patch $repo $branch", timeout => 60);
+
+    my $release_url = "https://github.com/jknphy/agama-integration-test-webpack/releases/latest/download/dist.tar.gz";
+    assert_script_run("agama download '$release_url' /tmp/dist.tar.gz");
+    assert_script_run("mkdir -p /usr/share/agama/system-tests");
+    assert_script_run("tar -xzf /tmp/dist.tar.gz -C /usr/share/agama/system-tests --strip-components=1");
 }
 
 1;
